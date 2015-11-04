a0c04f6d8aaae8e40fd371d04d726cfd3aaec354
SpoKen <spoken.spok@gmail.com>
2015-11-04 09:39:33 +0100

    ajout

---
+++ PartageRecette/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$_PAGES = array(1=>"acceuil", 
+				2=>"connexion", 
+				3=>"profil", 
+				4=>"recette_detail", 
+				5=>"recette_liste");
+
+$HOME_PAGE = "acceuil";
+$ERROR_404 = "acceuil";
+
+?>
---
+++ PartageRecette/index.php
@@ -0,0 +1,8 @@
+{include 'config.inc.php'}
+
+<?php
+	{$data = array()}
+	{$_GET = $current_page}
+	{for $_PAGES}
+
+?>
