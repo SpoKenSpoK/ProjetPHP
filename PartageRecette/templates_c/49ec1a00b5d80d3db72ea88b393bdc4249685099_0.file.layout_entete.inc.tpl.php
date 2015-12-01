9238e7c4d7e551feb61667c9a157732c95867900
SpoKen <spoken.spok@gmail.com>
2015-12-01 09:23:19 +0100

    rrerezra

---
+++ PartageRecette/templates_c/49ec1a00b5d80d3db72ea88b393bdc4249685099_0.file.layout_entete.inc.tpl.php
@@ -0,0 +1,37 @@
+<?php /* Smarty version 3.1.24, created on 2015-12-01 09:22:15
+         compiled from "C:/UwAmp/www/Projet_PHP/PartageRecette/application/views/layout/layout_entete.inc.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:12424565d58b75a9023_80074989%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '49ec1a00b5d80d3db72ea88b393bdc4249685099' => 
+    array (
+      0 => 'C:/UwAmp/www/Projet_PHP/PartageRecette/application/views/layout/layout_entete.inc.tpl',
+      1 => 1448955022,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '12424565d58b75a9023_80074989',
+  'has_nocache_code' => false,
+  'version' => '3.1.24',
+  'unifunc' => 'content_565d58b75b8848_88050679',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_565d58b75b8848_88050679')) {
+function content_565d58b75b8848_88050679 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '12424565d58b75a9023_80074989';
+?>
+<h1>WTC</h1>
+<nav>
+	<ul>
+		<li><a href="">Acceuil</a></li>
+		<li><a href="">Recettes</a></li>
+		<li><a href="">Espace Perso</a></li>
+	</ul>
+</nav>
+<?php }
+}
+?>
---
+++ PartageRecette/templates_c/7356f24521ad21e608158d68c7a3986473b8b535_0.file.layout_pied.inc.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version 3.1.24, created on 2015-12-01 09:22:24
+         compiled from "C:/UwAmp/www/Projet_PHP/PartageRecette/application/views/layout/layout_pied.inc.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:26461565d58c0142992_19319027%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7356f24521ad21e608158d68c7a3986473b8b535' => 
+    array (
+      0 => 'C:/UwAmp/www/Projet_PHP/PartageRecette/application/views/layout/layout_pied.inc.tpl',
+      1 => 1448958142,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '26461565d58c0142992_19319027',
+  'has_nocache_code' => false,
+  'version' => '3.1.24',
+  'unifunc' => 'content_565d58c0178606_56053046',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_565d58c0178606_56053046')) {
+function content_565d58c0178606_56053046 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '26461565d58c0142992_19319027';
+?>
+<p><b> created rrererreerre rrerererere erererer by Thibault HECKEL & Guillaume BOEHM </b></p>
+<?php }
+}
+?>
