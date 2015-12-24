4bbc59c18f0fd7895798bcb31ab42fc3ab9bcae4
Yorokobii <lennonaddicts@hotmail.fr>
2015-12-24 12:29:19 +0100

    bon il se passe des choses pour la connexion mais c'est pas encore ça... j'ai l'impression que la session ne se conserve pas si on change de page

--- PartageRecette/application/modules/connexion.inc.php
+++ PartageRecette/application/modules/connexion.inc.php
@@ -1,17 +1,37 @@
 <?php
-	if(isset($_REQUEST['login']) && isset($_REQUEST['mdp'])){
-		$login = $_REQUEST['login'];
-		$mdp = $_REQUEST['mdp'];
 
-		if(/*$login == /*(requete pour sortir ce login)*/ /*&& $mdp == /*(requete pour faire sortir juste le mot de passe de l'utilisateur $login)*/){
-			session_start();
-			$_SESSION['login'] = $login;
-			$_SESSION['mdp'] = $mdp;
+if(isset($_REQUEST['login']) && isset($_REQUEST['mdp'])){
 
-			header('location: ../../index.php?page=');
-		}
-		else{
-			header('location: ../../index.php?page=connexion');
-		}
+	/*************creation de l'acces bdd***************/
+	define('DB_HOST', 'localhost');
+	define('DB_DATABASE', 'recette');
+	define('DB_USERNAME', 'u_recette');
+	define('DB_PASSWORD', 'SJzEeqLb2HHeNYVV');
+
+	try{
+		$PDO_BDD = new PDO('mysql:host='.DB_HOST.';dbname='.DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+		$PDO_BDD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$PDO_BDD->exec("SET NAMES 'utf8'");
 	}
+	catch(Exception $e){
+		echo 'Erreur : '.$e->getMessage().'<br />';
+		echo 'N° : '.$e->getCode();
+		exit();
+	}
+
+	/***************************************************/
+
+	$login = $_REQUEST['login'];
+	$mdp = $_REQUEST['mdp'];
+
+	if(1/*(requete pour sortir ce login)*/ /*&& $mdp == /*(requete pour faire sortir juste le mot de passe de l'utilisateur $login)*/){
+		session_start();
+		$_SESSION['login'] = $login;
+
+		header('location: ../../index.php?page=liste');
+	}
+	else{
+		header('location: ../../index.php?page=connexion');
+	}
+}
 ?>
